95b1f7505b05af9ad449a322f11d9c62017abd93
Pruds52 <prudenciopvp@gmail.com>
2024-08-22 22:29:40 -0300

    Criação do logo do navbar

--- pw3-prudencio-app-libri/src/components/layout/NavBar.jsx
+++ pw3-prudencio-app-libri/src/components/layout/NavBar.jsx
@@ -5,6 +5,7 @@ const NavBar = () =>{
     return(
         <nav className={style.navbar}>
             <ul className={style.list}>
+                <li className={style.item}><img className={style.logo} src="./book.png"/></li>
                 <li className={style.item}>HOME</li>
                 <li className={style.item}>CADASTRAR LIVRO</li>
                 <li className={style.item}>LISTAR LIVRO</li>    
