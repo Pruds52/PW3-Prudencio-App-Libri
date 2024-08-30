63f08e45db4c00be5d7d2a4ccad361260c1ef51f
Pruds52 <prudenciopvp@gmail.com>
2024-08-29 22:21:58 -0300

    Criação das pages Home, CreateBooks e ListBooks; Criação das rotas

--- pw3-prudencio-app-libri/src/App.jsx
+++ pw3-prudencio-app-libri/src/App.jsx
@@ -1,12 +1,31 @@
 import "./App.css";
-import Pedro from "./components/layout/NavBar"
-// import CardBooks from "./components/CardBooks";
-// import capalivro from "./assets/livros/cavernas_aco.jpg" 
+import NavBar from "./components/layout/NavBar";
+import Container from "./components/layout/Container";
+import Home from "./components/pages/Home";
+import CreateBooks from "./components/pages/CreateBooks";
+import ListBooks from "./components/pages/ListBooks";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Pedro/>
+      <BrowserRouter>
+
+        <Container>
+
+          <Routes>
+
+            <Route path="/" element={<NavBar/>}>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/listBook" element={<ListBooks/>}/>
+              <Route path="/createBook" element={<CreateBooks/>}/>
+            </Route>
+
+          </Routes>
+          
+        </Container>
+
+      </BrowserRouter>
     </>
   );
 }
--- pw3-prudencio-app-libri/src/components/layout/NavBar.jsx
+++ pw3-prudencio-app-libri/src/components/layout/NavBar.jsx
@@ -1,17 +1,28 @@
-import style from "./NavBar.module.css"
+import { Outlet, Link } from "react-router-dom";
+import style from "./NavBar.module.css";
 
-const NavBar = () =>{
-    
-    return(
-        <nav className={style.navbar}>
-            <ul className={style.list}>
-                <li className={style.item}><img className={style.logo} src="./book.png"/></li>
-                <li className={style.item}>HOME</li>
-                <li className={style.item}>CADASTRAR LIVRO</li>
-                <li className={style.item}>LISTAR LIVRO</li>    
-            </ul>         
-        </nav>
-    )
-}
+const NavBar = () => {
+  return (
+    <>
+      <nav className={style.navbar}>
+        <ul className={style.list}>
+          <li className={style.item}>
+            <img className={style.logo} src="./book.png" />
+          </li>
+          <Link to="/">
+            <li className={style.item}>HOME</li>
+          </Link>
+          <Link to="/createBook">
+            <li className={style.item}>CADASTRAR LIVRO</li>
+          </Link>
+          <Link to="/listBook">
+            <li className={style.item}>LISTAR LIVRO</li>
+          </Link>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
 
-export default NavBar
+export default NavBar;
