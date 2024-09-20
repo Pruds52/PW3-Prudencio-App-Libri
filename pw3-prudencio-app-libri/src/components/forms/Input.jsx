13a0cad58447d41a76249ace4054006eeb44cb10
Pruds52 <prudenciopvp@gmail.com>
2024-09-19 22:26:46 -0300

    Criação da requisição GET de categorias na API

--- pw3-prudencio-app-libri/src/components/forms/Input.jsx
+++ pw3-prudencio-app-libri/src/components/forms/Input.jsx
@@ -1,6 +1,6 @@
 import styles from "./Input.module.css"
 
-function Input({type, text, name, placeHolder}){
+function Input({type, text, name, placeholder}){
     return(
         <div className={styles.form_control}>
 
@@ -9,7 +9,7 @@ function Input({type, text, name, placeHolder}){
             type={type}
             name={name}
             id={name}
-            placeHolder={placeHolder} />
+            placeholder={placeholder} />
 
         </div>
     )
--- pw3-prudencio-app-libri/src/components/pages/CreateBooks.jsx
+++ pw3-prudencio-app-libri/src/components/pages/CreateBooks.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import style from "./CreateBooks.module.css";
 import Input from "../forms/Input";
 import Select from "../forms/Select";
-import Button from "../forms/Button"
+import Button from "../forms/Button";
 
 const CreateBooks = () => {
+  useEffect(() => {
+    fetch("http://localhost:5000/listagemCateorias", {
+      method: "GET",
+      header: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*",
+      },
+    })
+      .then((resp) => 
+        // console.log("RESPOSTA: " + resp);
+        resp.json()
+      )
+      .then(
+        (data)=>
+          console.log("DATA: " + data.data[0].nome_categoria)
+        
+      ).catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <section className={style.create_book_container}>
       <h1>CREATE BOOKS</h1>
@@ -12,32 +35,27 @@ const CreateBooks = () => {
       <Input
         type="text"
         name="txt_livro"
-        placeHolder="Digite o nome do seu livro aqui"
+        placeholder="Digite o nome do seu livro aqui"
         text="Título do livro"
       />
 
       <Input
         type="text"
         name="txt_livro"
-        placeHolder="Digite o nome do autor"
+        placeholder="Digite o nome do autor"
         text="Nome do autor"
       />
 
       <Input
         type="text"
         name="txt_livro"
-        placeHolder="Digite a descrição do livro"
-        text="Descrição do livro"/>
-
-      <Select
-        name="categoria"
-        text="Escolha uma categoria de livro"
+        placeholder="Digite a descrição do livro"
+        text="Descrição do livro"
       />
 
-      <Button
-        rotulo="Cadastrar Livro"
-      />
+      <Select name="categoria" text="Escolha uma categoria de livro" />
 
+      <Button rotulo="Cadastrar Livro" />
     </section>
   );
 };
